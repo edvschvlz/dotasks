d5b3f4e1e87f71dd062b0cd994221a015091f4cb
Eduardo Schulz <eduardo.schulz68@gmail.com>
2023-07-25 19:57:28 -0300

    feat: show modal

--- frontend/src/Components/Modal/index.jsx
+++ frontend/src/Components/Modal/index.jsx
@@ -1,15 +1,15 @@
-import './';
+import './Modal.css';
 
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ show, setShowModal, children }) => {
   const showHideClassName = show ? 'modal display-block' : 'modal display-none';
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
+        <div className="button_close">
+          <i type="button" className="bi bi-x-lg" onClick={() => setShowModal(false)}></i>
+        </div>
         {children}
-        <button type="button" onClick={handleClose}>
-          Close
-        </button>
       </section>
     </div>
   );
